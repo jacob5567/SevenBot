0d3712855be1aea84227125d686640d181cddb13
Jacob Faulk <jacob5567@gmail.com>
2020-04-22 18:03:52 -0700

    changed to class syntax

--- SevenBot.py
+++ SevenBot.py
@@ -11,12 +11,14 @@ GUILD = os.getenv("DISCORD_GUILD")
 client = discord.Client()
 
 
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name} (id: {guild.id})'
-    )
+class CustomClient(discord.Client):
 
+    async def on_ready(self):
+        guild = discord.utils.get(self.guilds, name=GUILD)
+        print(
+            f'{self.user} is connected to the following guild:\n'
+            f'{guild.name} (id: {guild.id})'
+        )
+
+client = CustomClient()
 client.run(TOKEN)
