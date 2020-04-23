d9ece583898dcf88fe30411dc5cb44a7c6f39252
Jacob Faulk <jacob5567@gmail.com>
2020-04-23 15:28:06 -0700

    test reading JSON

--- SevenBot.py
+++ SevenBot.py
@@ -2,6 +2,7 @@
 
 import os
 import discord
+import json
 from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -17,11 +18,19 @@ async def sendMessage(channel_id, message_body):
 
 client = discord.Client()
 scheduler = AsyncIOScheduler()
-scheduler.add_job(sendMessage, 'cron', day_of_week="mon", hour=10)
+# scheduler.add_job(sendMessage, 'cron', day_of_week="mon", hour=10)
 
 
 @client.event
 async def on_ready():
+    f = open("messageinfo.json")
+    message_info = json.load(f)
+
+    for info in message_info["scheduled_messages"]:
+        print(info["day_of_week"])
+
+    f.close()
+
     scheduler.start()
 
     guild = discord.utils.get(client.guilds, name=GUILD)
